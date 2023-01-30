e4dfc45d865088c35f2c61cf14c787c917985bc6
Bruno <brunosilvabrait@gmail.com>
2023-01-30 19:29:46 -0300

    First commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::API
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
+end
