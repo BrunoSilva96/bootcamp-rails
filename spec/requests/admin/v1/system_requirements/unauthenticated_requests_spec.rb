086210d42fb46a493b947d9e3a1f5677f5a2fc28
Bruno <brunosilvabrait@gmail.com>
2023-02-14 18:59:02 -0300

    CRUD e Testes de System Requirements completo

---
+++ spec/requests/admin/v1/system_requirements/unauthenticated_requests_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::V1::SystemRequirement without authentication", type: :request do
+  let(:user) { create(:user, profile: :client) }
+
+  context "GET /system_requirements" do
+    let(:url) { "/admin/v1/system_requirements" }
+    let!(:system_requirement) { create_list(:system_requirement, 10) }
+
+    before(:each) { get url }
+
+    include_examples "unauthenticated access"
+  end
+
+  context "POST /system_requirements" do
+    let(:url) { "/admin/v1/system_requirements" }
+
+    before(:each) { post url }
+    include_examples "unauthenticated access"
+  end
+
+  context "PATCH /system_requirements/:id" do
+    let(:system_requirement) { create(:system_requirement) }
+    let(:url) { "/admin/v1/system_requirements/#{system_requirement.id}" }
+
+    before(:each) { patch url }
+    include_examples "unauthenticated access"
+  end
+
+  context "DELETE /system_requirements/:id" do
+    let(:system_requirement) { create(:system_requirement) }
+    let(:url) { "/admin/v1/system_requirements/#{system_requirement.id}" }
+
+    before(:each) { delete url }
+    include_examples "unauthenticated access"
+  end
+end
