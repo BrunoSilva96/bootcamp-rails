088992bab3f7d7e23a810e697c314f3a818504ba
Bruno <brunosilvabrait@gmail.com>
2023-02-27 19:15:16 -0300

    Refatoração da paginação, CRUD de License

---
+++ app/controllers/admin/v1/licenses_controller.rb
@@ -0,0 +1,12 @@
+module Admin::V1
+  class LicensesController < ApplicationController
+    def index
+    end
+    def create
+    end
+    def update
+    end
+    def destroy
+    end
+  end
+end
---
+++ spec/factories/licenses.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :license do
+    key { 1 }
+    game { nil }
+    user { nil }
+  end
+end
