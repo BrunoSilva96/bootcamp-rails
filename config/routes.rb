e62512f77cc0cb010ed38bc7667acc9968c934de
Bruno <brunosilvabrait@gmail.com>
2023-03-09 18:28:25 -0300

    Endpoint da busca de Produtos, Detalher do Produo, Lista de Categorias, Model para WishList

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   namespace :storefront do
     namespace :v1 do  
       get "home" => "home#index"
+      resources :products, only: [:index, :show]
+      resources :categories, only: [:index]
     end
   end
 end
