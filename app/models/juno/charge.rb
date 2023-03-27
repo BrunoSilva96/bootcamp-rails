27bf8f3c56528acac1a3c8f904d1f7d978214753
Bruno <brunosilvabrait@gmail.com>
2023-03-27 18:54:05 -0300

    Model de Pagamento  com Cartão do Juno

--- app/models/juno/charge.rb
+++ app/models/juno/charge.rb
@@ -1,11 +1,13 @@
 class Juno::Charge < ApplicationRecord
   belongs_to :order
-
+  
   validates :key, presence: true
   validates :code, presence: true
   validates :number, presence: true,
-                     uniqueness: { scope: :order_id },
-                     numericality: { only_integer: true, greater_than: 0 }
+  uniqueness: { scope: :order_id },
+  numericality: { only_integer: true, greater_than: 0 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :status, presence: true
+
+  has_many :credit_card_payments, class_name: 'Juno::CreditCardPayment'
 end
